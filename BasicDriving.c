eb50e9affc1cb90286f42ee0c651dc0a48f7df38
Louis Goessling <louis@goessling.com>
2013-11-20 07:59:41 -0600

    Added comments, Prettified Code

--- BasicDriving.c
+++ BasicDriving.c
@@ -1,7 +1,7 @@
-#pragma config(Hubs   , S1          , HTMotor , none                   , none     , none)
-#pragma config(Sensor , S1          ,         , sensorI2CMuxController                  )
-#pragma config(Motor  , mtr_S1_C1_1 , motor1  , tmotorTetrix           , openLoop       )
-#pragma config(Motor  , mtr_S1_C1_2 , motor2  , tmotorTetrix           , openLoop       )
+#pragma config(Hubs   , S1          , HTMotor , none                   , none     , none  )
+#pragma config(Sensor , S1          ,         , sensorI2CMuxController ,          ,       )
+#pragma config(Motor  , mtr_S1_C1_1 , motor1  , tmotorTetrix           , openLoop ,       )
+#pragma config(Motor  , mtr_S1_C1_2 , motor2  , tmotorTetrix           , openLoop ,       )
 //Setup Motors^
 
 //Allow Use Of Joysticks:
@@ -14,18 +14,16 @@ const int SPEED_MULT =1 ;
 
 //Main Code:
 task main(){
-	while(true){
-		if(abs(SPEED_MULT * joystick.joy1_y1) > THRESHOLD) {
-			motor[motor1] = SPEED_MULT * joystick.joy1_y1;
+	while(true){ //Repeat the following forever:
+		if(abs(SPEED_MULT * joystick.joy1_y1) > THRESHOLD) { //If the joystick is outside of the deadzone:
+			motor[motor1] = SPEED_MULT * joystick.joy1_y1;   //Set motor speed to joystick value * multiplier
+		}else{                                               //Otherwise:
+			motor[motor1] = 0;                               //Set motor to 0
 		}
-		else{
-			motor[motor1] = 0;
-		}
-		if(abs(SPEED_MULT * joystick.joy1_y2) > THRESHOLD) {
-			motor[motor2] = SPEED_MULT * joystick.joy1_y2;
-		}
-		else {
-			motor[motor2] = 0;
+		if(abs(SPEED_MULT * joystick.joy1_y2) > THRESHOLD) { //If the joystick is outside of the deadzone:
+			motor[motor2] = SPEED_MULT * joystick.joy1_y2;   //Set motor speed to joystick value * multiplier
+		}else{                                               //Otherwise:
+			motor[motor2] = 0;                               //Set motor to 0
 		}
 	}
 }
