2c96af2ec324bf8aa16bc9b09aab947fbf72beda
Louis Goessling <louis@goessling.com>
2013-11-20 07:51:43 -0600

    Prettify Code

--- BasicDriving.c
+++ BasicDriving.c
@@ -1,27 +1,30 @@
-//Motor Setup:
-#pragma config(Hubs   ,  S1          , HTMotor , none                   , none     , none )
-#pragma config(Sensor ,  S1          ,         , sensorI2CMuxController                   )
-#pragma config(Motor  ,  mtr_S1_C1_1 , motor1  , tmotorTetrix           , openLoop        )
-#pragma config(Motor  ,  mtr_S1_C1_2 , motor2  , tmotorTetrix           , openLoop        )
+#pragma config(Hubs   , S1          , HTMotor , none                   , none     , none)
+#pragma config(Sensor , S1          ,         , sensorI2CMuxController                  )
+#pragma config(Motor  , mtr_S1_C1_1 , motor1  , tmotorTetrix           , openLoop       )
+#pragma config(Motor  , mtr_S1_C1_2 , motor2  , tmotorTetrix           , openLoop       )
+//Setup Motors^
+
 //Allow Use Of Joysticks:
 #pragma  debuggerWindows("joystickSimple");
 #include "JoystickDriver.c"
 
 //Define Constants:
-const int THRESHOLD =10;
-const int SPEED_MULT=1 ;
+const int THRESHOLD  =10;
+const int SPEED_MULT =1 ;
 
 //Main Code:
 task main(){
 	while(true){
-		if(abs(joystick.joy1_y1) > THRESHOLD){
+		if(abs(SPEED_MULT * joystick.joy1_y1) > THRESHOLD) {
 			motor[motor1] = SPEED_MULT * joystick.joy1_y1;
-		}else{
+		}
+		else{
 			motor[motor1] = 0;
 		}
-		if(abs(joystick.joy1_y2) > THRESHOLD){
+		if(abs(SPEED_MULT * joystick.joy1_y2) > THRESHOLD) {
 			motor[motor2] = SPEED_MULT * joystick.joy1_y2;
-		}else{
+		}
+		else {
 			motor[motor2] = 0;
 		}
 	}
