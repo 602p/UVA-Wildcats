1e3590e9dba4f4039d36cdfd9ed77ca381452589
Louis Goessling <louis@goessling.com>
2013-11-22 16:25:13 -0600

    Reversed a motor so it will drive forward

--- BasicDriving.c
+++ BasicDriving.c
@@ -1,7 +1,7 @@
-#pragma config(Hubs   , S1          , HTMotor , none                   , none     , none  )
-#pragma config(Sensor , S1          ,         , sensorI2CMuxController ,          ,       )
-#pragma config(Motor  , mtr_S1_C1_1 , motor1  , tmotorTetrix           , openLoop ,       )
-#pragma config(Motor  , mtr_S1_C1_2 , motor2  , tmotorTetrix           , openLoop ,       )
+#pragma config(Hubs   , S1          , HTMotor , none                   , none     , none     )
+#pragma config(Sensor , S1          ,         , sensorI2CMuxController ,          ,          )
+#pragma config(Motor  , mtr_S1_C1_1 , motor1  , tmotorTetrix           , openLoop , reversed )
+#pragma config(Motor  , mtr_S1_C1_2 , motor2  , tmotorTetrix           , openLoop ,          )
 //Setup Motors^
 
 //========================================//
