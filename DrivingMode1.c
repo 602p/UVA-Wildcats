9569618f79c8d3587444f60f0e91e439d81e3859
Louis Goessling <louis@goessling.com>
2013-11-26 17:26:24 -0600

    Switched motors, darn build team; Organized drive code

--- DrivingMode1.c
+++ DrivingMode1.c
@@ -2,10 +2,10 @@
 #pragma config(Sensor,  S1         ,           , sensorI2CMuxController                     )
 #pragma config(Motor ,  mtr_S1_C1_1, leftDrive , tmotorTetrix          , openLoop, reversed )
 #pragma config(Motor ,  mtr_S1_C1_2, rightDrive, tmotorTetrix          , openLoop           )
-#pragma config(Motor ,  mtr_S1_C2_1, arm       , tmotorTetrix          , openLoop           )
+#pragma config(Motor ,  mtr_S1_C3_2, arm       , tmotorTetrix          , openLoop           )
 #pragma config(Motor ,  mtr_S1_C2_2, elbow     , tmotorTetrix          , openLoop           )
 #pragma config(Motor ,  mtr_S1_C3_1, spinner   , tmotorTetrix          , openLoop           )
-#pragma config(Motor ,  mtr_S1_C3_2, gripper   , tmotorTetrix          , openLoop           )
+#pragma config(Motor ,  mtr_S1_C2_1, gripper   , tmotorTetrix          , openLoop           )
 //Setup Motors^
 
 //For control mappings refer to /notes.txt
@@ -21,15 +21,22 @@ const int SPEED_MULT =1 ;
 //Main Code:
 task main(){
 	while(true){ //Repeat the following forever:
-		if(abs(SPEED_MULT * joystick.joy1_y1) > THRESHOLD) { //If the joystick is outside of the deadzone:
-			motor[leftDrive] = SPEED_MULT * joystick.joy1_y1;   //Set motor speed to joystick value * multiplier
-		}else{                                               //Otherwise:
-			motor[leftDrive] = 0;                               //Set motor to 0
+
+		{ // Driving code:
+			if(abs(SPEED_MULT * joystick.joy1_y1) > THRESHOLD) { //If the joystick is outside of the deadzone:
+				motor[leftDrive] = SPEED_MULT * joystick.joy1_y1;   //Set motor speed to joystick value * multiplier
+			}else{                                               //Otherwise:
+				motor[leftDrive] = 0;                               //Set motor to 0
+			}
+			if(abs(SPEED_MULT * joystick.joy1_y2) > THRESHOLD) { //If the joystick is outside of the deadzone:
+				motor[rightDrive] = SPEED_MULT * joystick.joy1_y2;   //Set motor speed to joystick value * multiplier
+			}else{                                               //Otherwise:
+				motor[rightDrive] = 0;                               //Set motor to 0
+			}
 		}
-		if(abs(SPEED_MULT * joystick.joy1_y2) > THRESHOLD) { //If the joystick is outside of the deadzone:
-			motor[rightDrive] = SPEED_MULT * joystick.joy1_y2;   //Set motor speed to joystick value * multiplier
-		}else{                                               //Otherwise:
-			motor[rightDrive] = 0;                               //Set motor to 0
+
+		{
+			
 		}
 	}
 }
