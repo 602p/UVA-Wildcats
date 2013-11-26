906d9526ed50e1421ecf57cc3d974105ea1c8c59
Louis Goessling <louis@goessling.com>
2013-11-26 16:33:09 -0600

    Added DrivingMode1

---
+++ DrivingMode1.c
@@ -0,0 +1,31 @@
+#pragma config(Hubs   , S1          , HTMotor , none                   , none     , none     )
+#pragma config(Sensor , S1          ,         , sensorI2CMuxController ,          ,          )
+#pragma config(Motor  , mtr_S1_C1_1 , motor1  , tmotorTetrix           , openLoop , reversed )
+#pragma config(Motor  , mtr_S1_C1_2 , motor2  , tmotorTetrix           , openLoop ,          )
+//Setup Motors^
+
+//For control mappings refer to /notes.txt
+
+//Allow Use Of Joysticks:
+#pragma  debuggerWindows("joystickSimple");
+#include "JoystickDriver.c"
+
+//Define Constants:
+const int THRESHOLD  =10;
+const int SPEED_MULT =1 ;
+
+//Main Code:
+task main(){
+	while(true){ //Repeat the following forever:
+		if(abs(SPEED_MULT * joystick.joy1_y1) > THRESHOLD) { //If the joystick is outside of the deadzone:
+			motor[motor1] = SPEED_MULT * joystick.joy1_y1;   //Set motor speed to joystick value * multiplier
+		}else{                                               //Otherwise:
+			motor[motor1] = 0;                               //Set motor to 0
+		}
+		if(abs(SPEED_MULT * joystick.joy1_y2) > THRESHOLD) { //If the joystick is outside of the deadzone:
+			motor[motor2] = SPEED_MULT * joystick.joy1_y2;   //Set motor speed to joystick value * multiplier
+		}else{                                               //Otherwise:
+			motor[motor2] = 0;                               //Set motor to 0
+		}
+	}
+}
